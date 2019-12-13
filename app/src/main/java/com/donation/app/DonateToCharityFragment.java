3618d568e9d73884dfdd5735660162b008dbb735
Gonzales Sarah Jane <sarah_jane.gonzales@nokia.com>
2019-12-13 18:27:46 +0800

    initial

---
+++ app/src/main/java/com/donation/app/DonateToCharityFragment.java
@@ -0,0 +1,80 @@
+package com.donation.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.donation.app.R;
+import com.donation.app.ui.main.PageViewModel;
+import com.donation.app.ui.main.PlaceholderFragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DonateToCharityFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static DonateToCharityFragment newInstance(int index) {
+        DonateToCharityFragment fragment = new DonateToCharityFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+//        int index = 1;
+//        if (getArguments() != null) {
+//            index = getArguments().getInt(ARG_SECTION_NUMBER);
+//        }
+//        pageViewModel.setIndex(index);
+//    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_donate_to_charity, container, false);
+//        final TextView textView = root.findViewById(R.id.section_label);
+//        pageViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        Button b = (Button) root.findViewById(R.id.button5);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findCharities(v);
+            }
+        });
+        return root;
+    }
+
+
+    /** Called when the user taps the Send button */
+    public void findCharities(View view) {
+        Intent intent = new Intent(getActivity(), FindCharityActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/donation/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.donation.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void login(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+}
