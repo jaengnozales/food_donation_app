59135da4a04bb7c0a11e9971921c5a7f19668fc8
Sarah G <jaengonzales@gmail.com>
2019-12-16 00:42:31 +0800

    donate button

--- app/src/main/java/com/donation/app/FindCharityActivity.java
+++ app/src/main/java/com/donation/app/FindCharityActivity.java
@@ -2,13 +2,20 @@ package com.donation.app;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class FindCharityActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -43,5 +50,80 @@ public class FindCharityActivity extends FragmentActivity implements OnMapReadyC
         LatLng sydney = new LatLng(14, 120);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Manila"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+    }
+
+    /** Demonstrates customizing the info window and/or its contents. */
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        // These are both viewgroups containing an ImageView with id "badge" and two TextViews with id
+        // "title" and "snippet".
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            render(marker, mContents);
+            return mContents;
+        }
+
+        private void render(Marker marker, View view) {
+//            int badge;
+//            // Use the equals() method on a Marker to check for equals.  Do not use ==.
+//            if (marker.equals(mBrisbane)) {
+//                badge = R.drawable.badge_qld;
+//            } else if (marker.equals(mAdelaide)) {
+//                badge = R.drawable.badge_sa;
+//            } else if (marker.equals(mSydney)) {
+//                badge = R.drawable.badge_nsw;
+//            } else if (marker.equals(mMelbourne)) {
+//                badge = R.drawable.badge_victoria;
+//            } else if (marker.equals(mPerth)) {
+//                badge = R.drawable.badge_wa;
+//            } else if (marker.equals(mDarwin1)) {
+//                badge = R.drawable.badge_nt;
+//            } else if (marker.equals(mDarwin2)) {
+//                badge = R.drawable.badge_nt;
+//            } else if (marker.equals(mDarwin3)) {
+//                badge = R.drawable.badge_nt;
+//            } else if (marker.equals(mDarwin4)) {
+//                badge = R.drawable.badge_nt;
+//            } else {
+//                // Passing 0 to setImageResource will clear the image view.
+//                badge = 0;
+//            }
+//            ((ImageView) view.findViewById(R.id.badge)).setImageResource(badge);
+
+            String title = marker.getTitle();
+            TextView titleUi = ((TextView) view.findViewById(R.id.title));
+            if (title != null) {
+                // Spannable string allows us to edit the formatting of the text.
+                SpannableString titleText = new SpannableString(title);
+                titleText.setSpan(new ForegroundColorSpan(Color.RED), 0, titleText.length(), 0);
+                titleUi.setText(titleText);
+            } else {
+                titleUi.setText("");
+            }
+
+            String snippet = marker.getSnippet();
+            TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+            if (snippet != null && snippet.length() > 12) {
+                SpannableString snippetText = new SpannableString(snippet);
+                snippetText.setSpan(new ForegroundColorSpan(Color.MAGENTA), 0, 10, 0);
+                snippetText.setSpan(new ForegroundColorSpan(Color.BLUE), 12, snippet.length(), 0);
+                snippetUi.setText(snippetText);
+            } else {
+                snippetUi.setText("");
+            }
+        }
     }
 }
