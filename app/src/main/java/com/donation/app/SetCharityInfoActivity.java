2d978eb09c2ab0108eb47f84c99033bf83a0d3c3
Meren <giltroy.meren@nokia.com>
2019-12-16 15:30:48 +0800

    Test

---
+++ app/src/main/java/com/donation/app/SetCharityInfoActivity.java
@@ -0,0 +1,14 @@
+package com.donation.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetCharityInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_charity_info);
+    }
+}
