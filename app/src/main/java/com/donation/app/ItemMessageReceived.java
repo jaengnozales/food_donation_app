e7e25e9536d47549cf8bf111353352e137f8a695
Gonzales Sarah Jane <sarah_jane.gonzales@nokia.com>
2019-12-17 14:17:50 +0800

    received message

---
+++ app/src/main/java/com/donation/app/ItemMessageReceived.java
@@ -0,0 +1,14 @@
+package com.donation.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemMessageReceived extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_message_received);
+    }
+}
